ed4247ac9eff7e6199a11e8bff37403a535f89c5
kkupgrader <kkupgrader.fs@gmail.com>
2022-12-22 10:37:30 +0530

    some login bugs fixed

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -9,7 +9,7 @@ font-weight: 400;
 font-size: 14px;
 color:#fbfbfb;
 padding:26px 16px;
-background:${props => props.type.length > 0 ? (props.type == "success" ? "#6FCF97" : props.type == "error" ? "#EB5757" : "#E0E0E0") : "#E0E0E0"};
+background:${props => props.type ? (props.type == "success" ? "#6FCF97" : props.type == "error" ? "#EB5757" : "#E0E0E0") : "#E0E0E0"};
 display:${props => props.hidden ? "none" : "block"}
 `;
 export default Alert;
